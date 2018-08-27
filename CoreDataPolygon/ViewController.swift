7c517cdaa02c9059a22ca23852537100fb5b4acc
m4riusz <sariuszm@gmail.com>
2018-08-27 19:55:40 +0200

    init

--- CoreDataPolygon/ViewController.swift
+++ CoreDataPolygon/ViewController.swift
@@ -1,24 +1,6 @@
-//
-//  ViewController.swift
-//  CoreDataPolygon
-//
-//  Created by Mariusz Sut on 27.08.2018.
-//  Copyright © 2018 Mariusz Sut. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
